93ac7fa043cd881b1e8737d96fcf9acc0853ef51
Paolo Lalap <paolomorales1502@gmail.com>
2024-04-21 23:02:49 +0800

    add constants.js

---
+++ constants.js
@@ -0,0 +1,24 @@
+export const LAGUNA = [
+  "Git",
+  "Gitlab",
+  "MongoDB",
+  "Next-Auth",
+  "NextJs",
+  "NodeJs",
+  "React-hook form",
+  "Shadcn",
+  "Tailwind",
+];
+
+export const SYDNEY = [
+  "Firebase",
+  "Git",
+  "Github",
+  "Material UI",
+  "NextJs",
+  "NodeJs",
+  "React-hook-form",
+  "Shadcn",
+  "Tailwind",
+];
+
