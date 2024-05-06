693634302caa19cf6f54a0c12a25b8b4e0f87481
Paolo Lalap <paolomorales1502@gmail.com>
2024-05-06 14:46:57 +0800

    polished

--- components/SkillSet.jsx
+++ components/SkillSet.jsx
@@ -1,138 +1,20 @@
 "use client";
 import { Tilt } from "@jdion/tilt-react";
+import { SKILLS } from "@/constants/SKILLS";
 
 export default function SkillSet() {
   return (
-    <div className="mt-5 grid grid-cols-4 gap-4">
-      {/* ReactJs */}
-      <Tilt className="group w-full rounded-md border p-5 transition-all hover:rounded-lg hover:border-4 hover:bg-[#1b1d1c]">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="size-full text-white transition-colors group-hover:text-[#00d9ff]"
+    <div className="mt-5 grid size-full grid-cols-3 gap-4 md:grid-cols-4">
+      {SKILLS.map((skill) => (
+        <Tilt
+          key={skill.id}
+          className={
+            "group w-full rounded-md border p-5 transition-all hover:rounded-lg hover:border-4"
+          }
         >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M6.306 8.711c-2.602 .723 -4.306 1.926 -4.306 3.289c0 2.21 4.477 4 10 4c.773 0 1.526 -.035 2.248 -.102" />
-          <path d="M17.692 15.289c2.603 -.722 4.308 -1.926 4.308 -3.289c0 -2.21 -4.477 -4 -10 -4c-.773 0 -1.526 .035 -2.25 .102" />
-          <path d="M6.305 15.287c-.676 2.615 -.485 4.693 .695 5.373c1.913 1.105 5.703 -1.877 8.464 -6.66c.387 -.67 .733 -1.339 1.036 -2" />
-          <path d="M17.694 8.716c.677 -2.616 .487 -4.696 -.694 -5.376c-1.913 -1.105 -5.703 1.877 -8.464 6.66c-.387 .67 -.733 1.34 -1.037 2" />
-          <path d="M12 5.424c-1.925 -1.892 -3.82 -2.766 -5 -2.084c-1.913 1.104 -1.226 5.877 1.536 10.66c.386 .67 .793 1.304 1.212 1.896" />
-          <path d="M12 18.574c1.926 1.893 3.821 2.768 5 2.086c1.913 -1.104 1.226 -5.877 -1.536 -10.66c-.375 -.65 -.78 -1.283 -1.212 -1.897" />
-          <path d="M11.5 12.866a1 1 0 1 0 1 -1.732a1 1 0 0 0 -1 1.732z" />
-        </svg>
-      </Tilt>
-
-      {/* NextJs */}
-      <Tilt className="w-full rounded-md border p-5 transition-all hover:rounded-lg hover:border-4 hover:bg-[#111] hover:invert">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="size-full text-white"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M9 15v-6l7.745 10.65a9 9 0 1 1 2.255 -1.993" />
-          <path d="M15 12v-3" />
-        </svg>
-      </Tilt>
-
-      {/* TailwindCSS */}
-      <Tilt className="group w-full rounded-md border p-5 transition-all hover:rounded-lg hover:border-4">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="size-full text-white transition-colors group-hover:text-[#36bef8]"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M11.667 6c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 2 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968zm-4 6.5c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 1.975 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968z" />
-        </svg>{" "}
-      </Tilt>
-
-      {/* Git */}
-      <Tilt className="group w-full rounded-md border p-5 transition-all hover:rounded-lg hover:border-4">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="size-full text-white transition-colors group-hover:text-[#f05030]"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M16 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
-          <path d="M12 8m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
-          <path d="M12 16m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
-          <path d="M12 15v-6" />
-          <path d="M15 11l-2 -2" />
-          <path d="M11 7l-1.9 -1.9" />
-          <path d="M13.446 2.6l7.955 7.954a2.045 2.045 0 0 1 0 2.892l-7.955 7.955a2.045 2.045 0 0 1 -2.892 0l-7.955 -7.955a2.045 2.045 0 0 1 0 -2.892l7.955 -7.955a2.045 2.045 0 0 1 2.892 0z" />
-        </svg>
-      </Tilt>
-
-      {/* MongoDB */}
-      <Tilt className="group w-full rounded-md border p-5 transition-all hover:rounded-lg hover:border-4">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="size-full text-white transition-colors group-hover:text-[#07ac4e]"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M12 3v19" />
-          <path d="M18 11.227c0 3.273 -1.812 4.77 -6 9.273c-4.188 -4.503 -6 -6 -6 -9.273c0 -4.454 3.071 -6.927 6 -9.227c2.929 2.3 6 4.773 6 9.227z" />
-        </svg>
-      </Tilt>
-
-      {/* Firebase */}
-      <Tilt className="group w-full rounded-md border p-5 transition-all hover:rounded-lg hover:border-4">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="size-full text-white group-hover:text-[#ffc928]"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M4.53 17.05l6.15 -11.72h-.02c.38 -.74 1.28 -1.02 2.01 -.63c.26 .14 .48 .36 .62 .62l1.06 2.01" />
-          <path d="M15.47 6.45c.58 -.59 1.53 -.59 2.11 -.01c.22 .22 .36 .5 .41 .81l1.5 9.11c.1 .62 -.2 1.24 -.76 1.54l-6.07 2.9c-.46 .25 -1.01 .26 -1.46 0l-6.02 -2.92c-.55 -.31 -.85 -.92 -.75 -1.54l1.96 -12.04c.12 -.82 .89 -1.38 1.7 -1.25c.46 .07 .87 .36 1.09 .77l1.24 1.76" />
-          <path d="M4.57 17.18l10.93 -10.68" />
-        </svg>{" "}
-      </Tilt>
+          {skill.icon}
+        </Tilt>
+      ))}
     </div>
   );
 }
