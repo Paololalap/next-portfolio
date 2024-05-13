12428ec82634511fc6e7ef254a7f4fe7a86b90b3
Paolo Lalap <paolomorales1502@gmail.com>
2024-05-13 11:14:50 +0800

    update

--- components/SkillSet.jsx
+++ components/SkillSet.jsx
@@ -1,20 +1,119 @@
 "use client";
 import { Tilt } from "@jdion/tilt-react";
-import { SKILLS } from "@/constants/SKILLS";
 
 export default function SkillSet() {
   return (
     <div className="mt-5 grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
-      {SKILLS.map((skill) => (
-        <Tilt
-          key={skill.id}
-          className={
-            "group hidden rounded-md px-5 ring-1 ring-white transition-all hover:rounded-lg hover:ring-4 md:block"
-          }
-        >
-          {skill.icon}
-        </Tilt>
-      ))}
+      <Tilt
+        className={
+          "group grid place-items-center rounded-md px-5 ring-1 ring-white transition-all hover:rounded-lg hover:ring-4"
+        }
+      >
+        <svg className={"inline size-full self-center text-white"}>
+          <use
+            href={`./icons/homepage-sprite.svg#tabler/brand-react-outline`}
+          />
+        </svg>
+      </Tilt>
+      <Tilt
+        className={
+          "group grid place-items-center rounded-md px-5 ring-1 ring-white transition-all hover:rounded-lg hover:ring-4"
+        }
+      >
+        <svg className={"inline size-full self-center text-white"}>
+          <use
+            href={`./icons/homepage-sprite.svg#tabler/brand-nextjs-outline`}
+          />
+        </svg>
+      </Tilt>
+      <Tilt
+        className={
+          "group grid place-items-center rounded-md px-5 ring-1 ring-white transition-all hover:rounded-lg hover:ring-4"
+        }
+      >
+        <svg className={"inline size-full self-center text-white"}>
+          <use
+            href={`./icons/homepage-sprite.svg#tabler/brand-tailwind-outline`}
+          />
+        </svg>
+      </Tilt>
+      <Tilt
+        className={
+          "group grid place-items-center rounded-md px-5 ring-1 ring-white transition-all hover:rounded-lg hover:ring-4"
+        }
+      >
+        <svg className={"inline size-full self-center text-white"}>
+          <use
+            href={`./icons/homepage-sprite.svg#tabler/brand-git-outline`}
+          />
+        </svg>
+      </Tilt>
+      <Tilt
+        className={
+          "group grid place-items-center rounded-md px-5 ring-1 ring-white transition-all hover:rounded-lg hover:ring-4"
+        }
+      >
+        <svg className={"inline size-full self-center text-white"}>
+          <use
+            href={`./icons/homepage-sprite.svg#tabler/brand-mongodb-outline`}
+          />
+        </svg>
+      </Tilt>
+      <Tilt
+        className={
+          "group grid place-items-center rounded-md px-5 ring-1 ring-white transition-all hover:rounded-lg hover:ring-4"
+        }
+      >
+        <svg className={"inline size-full self-center text-white"}>
+          <use
+            href={`./icons/homepage-sprite.svg#tabler/brand-firebase-outline`}
+          />
+        </svg>
+      </Tilt>
+      <Tilt
+        className={
+          "group grid place-items-center rounded-md px-5 ring-1 ring-white transition-all hover:rounded-lg hover:ring-4"
+        }
+      >
+        <svg className={"inline size-full self-center text-white"}>
+          <use
+            href={`./icons/homepage-sprite.svg#tabler/brand-css3-outline`}
+          />
+        </svg>
+      </Tilt>
+      <Tilt
+        className={
+          "group grid place-items-center rounded-md px-5 ring-1 ring-white transition-all hover:rounded-lg hover:ring-4"
+        }
+      >
+        <svg className={"inline size-full self-center text-white"}>
+          <use
+            href={`./icons/homepage-sprite.svg#tabler/brand-html5-outline`}
+          />
+        </svg>
+      </Tilt>
+      <Tilt
+        className={
+          "group grid place-items-center rounded-md px-5 ring-1 ring-white transition-all hover:rounded-lg hover:ring-4"
+        }
+      >
+        <svg className={"inline size-full self-center text-white"}>
+          <use
+            href={`./icons/homepage-sprite.svg#tabler/brand-nodejs-outline`}
+          />
+        </svg>
+      </Tilt>
+      <Tilt
+        className={
+          "group grid place-items-center rounded-md px-5 ring-1 ring-white transition-all hover:rounded-lg hover:ring-4"
+        }
+      >
+        <svg className={"inline size-full self-center text-white"}>
+          <use
+            href={`./icons/homepage-sprite.svg#tabler/brand-npm-outline`}
+          />
+        </svg>
+      </Tilt>
     </div>
   );
 }
--- components/Tools.jsx
+++ components/Tools.jsx
@@ -1,20 +1,53 @@
 "use client";
 import { Tilt } from "@jdion/tilt-react";
-import { TOOLS } from "@/constants/TOOLS";
 
 export default function Tools() {
   return (
     <div className="mt-5 grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
-      {TOOLS.map((tool) => (
-        <Tilt
-          key={tool.id}
-          className={
-            "group rounded-md px-5 ring-1 ring-white transition-all hover:rounded-lg hover:ring-4"
-          }
-        >
-          {tool.icon}
-        </Tilt>
-      ))}
+      <Tilt
+        className={
+          "group rounded-md px-5 ring-1 ring-white transition-all hover:rounded-lg hover:ring-4"
+        }
+      >
+        <svg className={"inline size-full self-center text-white"}>
+          <use
+            href={`./icons/homepage-sprite.svg#tabler/brand-vscode-outline`}
+          />
+        </svg>
+      </Tilt>
+      <Tilt
+        className={
+          "group rounded-md px-5 ring-1 ring-white transition-all hover:rounded-lg hover:ring-4"
+        }
+      >
+        <svg className={"inline size-full self-center text-white"}>
+          <use
+            href={`./icons/homepage-sprite.svg#tabler/brand-vercel-outline`}
+          />
+        </svg>
+      </Tilt>
+      <Tilt
+        className={
+          "group rounded-md px-5 ring-1 ring-white transition-all hover:rounded-lg hover:ring-4"
+        }
+      >
+        <svg className={"inline size-full self-center text-white"}>
+          <use
+            href={`./icons/homepage-sprite.svg#tabler/brand-gitlab-outline`}
+          />
+        </svg>
+      </Tilt>
+      <Tilt
+        className={
+          "group rounded-md px-5 ring-1 ring-white transition-all hover:rounded-lg hover:ring-4"
+        }
+      >
+        <svg className={"inline size-full self-center text-white"}>
+          <use
+            href={`./icons/homepage-sprite.svg#tabler/brand-tabler-outline`}
+          />
+        </svg>
+      </Tilt>
     </div>
   );
 }
