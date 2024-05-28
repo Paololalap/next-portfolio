a703a1df11f6340d04b0f12dac3f395ddd067dba
Paolo Lalap <paolomorales1502@gmail.com>
2024-05-28 15:53:01 +0800

    update preload-resources

--- app/preload-resources.jsx
+++ app/preload-resources.jsx
@@ -1,11 +1,14 @@
 "use client";
 
-import { RESOURCES } from "@/constants/RESOURCES.js";
+import { useEffect } from "react";
 import ReactDOM from "react-dom";
 
 export function PreloadResources() {
-  RESOURCES.forEach((resource) => {
-    ReactDOM.preload(resource, { as: "image" });
-  });
+  useEffect(() => {
+    ReactDOM.preload("/icons/sprite.svg", {
+      as: "image",
+    });
+  }, []);
+
   return null;
 }
