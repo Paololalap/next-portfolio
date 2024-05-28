fddcd0f81b79c6a25430558af3e874ed5ba86bd1
Paolo Lalap <paolomorales1502@gmail.com>
2024-05-28 10:32:24 +0800

    update fonts.js

--- fonts/fonts.js
+++ fonts/fonts.js
@@ -4,7 +4,6 @@ export const roboto = Roboto({
   subsets: ["latin"],
   display: "swap",
   weight: ["100", "300", "400", "500", "700", "900"],
-  variable: "--roboto-font",
 });
 
 export const lobster = Lobster({
