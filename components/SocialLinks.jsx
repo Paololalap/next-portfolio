ce40bd60fc9d4fc4a408b27cb120f3094585a8bf
Paolo Lalap <paolomorales1502@gmail.com>
2024-06-07 22:15:22 +0800

    update button

--- components/SocialLinks.jsx
+++ components/SocialLinks.jsx
@@ -1,5 +1,6 @@
 import { SOCIAL_LINKS } from "@/constants/SOCIAL_LINKS";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 export default function SocialLinks() {
   return (
@@ -9,18 +10,23 @@ export default function SocialLinks() {
           <Link
             href={link.href}
             target="_blank"
-            className="group space-x-1 text-zinc-400 transition-all after:ml-3 hover:text-white"
+            tabIndex="-1"
+            className="space-x-1 transition-all group text-zinc-400 after:ml-3 hover:text-white"
           >
-            <svg
-              className={
-                "inline size-6 self-center text-zinc-400 transition-all group-hover:text-white"
-              }
+            <Button
+              variant="linkHover1"
+              className="p-0 text-base text-zinc-400 group-hover:text-white"
+              aria-label={link.text}
             >
-              {link.icon}
-            </svg>
-            <span className="border-b-2 border-zinc-400 text-zinc-400 transition-all group-hover:border-white group-hover:text-white">
+              <svg
+                className={
+                  "inline size-6 self-center text-zinc-400 transition-all group-hover:text-white"
+                }
+              >
+                {link.icon}
+              </svg>
               {link.text}
-            </span>
+            </Button>
           </Link>
         </li>
       ))}
