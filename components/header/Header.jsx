6cd4c73e8fda3c99b155af82d98bdbdbaec4b087
Paolo Lalap <paolomorales1502@gmail.com>
2024-06-08 22:35:52 +0800

    update header component

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,10 +1,11 @@
 "use client";
 
+import HoverLine from "@/components/HoverLine";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
-import HoverLine from "@/components/HoverLine";
 
 export default function Header() {
   const pathname = usePathname();
@@ -14,24 +15,30 @@ export default function Header() {
   const [showMenu, setShowMenu] = useState(true);
 
   return (
-    <header className="mx-auto mt-8 max-w-2xl">
-      <div className="flex items-center justify-between rounded-full border border-zinc-400 px-2 py-1 sm:flex-row sm:py-2">
-        <nav className="group relative hidden items-center text-base sm:flex">
+    <header className="max-w-2xl mx-auto mt-8">
+      <div className="flex items-center justify-between px-2 py-1 border rounded-full border-zinc-400 sm:flex-row sm:py-2">
+        <nav className="relative items-center hidden text-base group sm:flex">
           <ul className="flex">
             <li>
               <Link
                 href={"/"}
-                className="group/button relative inline-flex h-8 items-center px-3 text-sm text-zinc-400 hover:no-underline focus:rounded-full focus:outline-0 focus:ring-0"
+                className="relative inline-flex items-center h-8 px-3 text-sm group/button text-zinc-400 hover:no-underline focus:rounded-full focus:outline-0 focus:ring-0"
                 onMouseEnter={() => setHoveredButton("home")}
               >
                 <svg
-                  className={
-                    "mr-1 size-5 transition-all duration-500 group-hover/button:text-white"
-                  }
+                  className={cn(
+                    "mr-1 size-5 transition-all duration-500 group-hover/button:text-white",
+                    pathname === "/" && "text-white",
+                  )}
                 >
                   <use href={`./icons/sprite.svg#tabler/home-outline`} />
                 </svg>
-                <span className="transition-all duration-500 group-hover/button:text-white">
+                <span
+                  className={cn(
+                    "transition-all duration-500 group-hover/button:text-white",
+                    pathname === "/" && "text-white",
+                  )}
+                >
                   Home
                 </span>
               </Link>
@@ -41,16 +48,22 @@ export default function Header() {
                 href={"/work"}
                 tabIndex={"-1"}
                 onMouseEnter={() => setHoveredButton("work")}
-                className="group/button relative inline-flex h-8 items-center px-3 text-sm text-zinc-400 hover:no-underline focus:rounded-full focus:outline-0 focus:ring-0"
+                className="relative inline-flex items-center h-8 px-3 text-sm group/button text-zinc-400 hover:no-underline focus:rounded-full focus:outline-0 focus:ring-0"
               >
                 <svg
-                  className={
-                    "mr-1 size-5 transition-all duration-500 group-hover/button:text-white"
-                  }
+                  className={cn(
+                    "mr-1 size-5 transition-all duration-500 group-hover/button:text-white",
+                    pathname === "/work" && "text-white",
+                  )}
                 >
                   <use href={`./icons/sprite.svg#tabler/briefcase-2-outline`} />
                 </svg>{" "}
-                <span className="transition-all duration-500 group-hover/button:text-white">
+                <span
+                  className={cn(
+                    "transition-all duration-500 group-hover/button:text-white",
+                    pathname === "/work" && "text-white",
+                  )}
+                >
                   Work
                 </span>
               </Link>
@@ -60,16 +73,22 @@ export default function Header() {
                 href={"/projects"}
                 tabIndex={"-1"}
                 onMouseEnter={() => setHoveredButton("projects")}
-                className="group/button relative inline-flex h-8 items-center px-3 text-sm text-zinc-400 hover:no-underline focus:rounded-full focus:outline-0 focus:ring-0"
+                className="relative inline-flex items-center h-8 px-3 text-sm group/button text-zinc-400 hover:no-underline focus:rounded-full focus:outline-0 focus:ring-0"
               >
                 <svg
-                  className={
-                    "mr-1 size-5 transition-all duration-500 group-hover/button:text-white"
-                  }
+                  className={cn(
+                    "mr-1 size-5 transition-all duration-500 group-hover/button:text-white",
+                    pathname === "/projects" && "text-white",
+                  )}
                 >
                   <use href={`./icons/sprite.svg#tabler/folder-outline`} />
                 </svg>{" "}
-                <span className="transition-all duration-500 group-hover/button:text-white">
+                <span
+                  className={cn(
+                    "transition-all duration-500 group-hover/button:text-white",
+                    pathname === "/projects" && "text-white",
+                  )}
+                >
                   Projects
                 </span>
               </Link>
@@ -79,11 +98,11 @@ export default function Header() {
         </nav>
 
         <Button
-          className="bg-transparent p-0 hover:bg-transparent sm:hidden"
+          className="p-0 bg-transparent hover:bg-transparent sm:hidden"
           aria-label="menu"
           onClick={() => setShowMenu(!showMenu)}
         >
-          <svg className="ml-1 size-7 text-white">
+          <svg className="ml-1 text-white size-7">
             <use href={`./icons/sprite.svg#tabler/menu-2-outline`} />
           </svg>
         </Button>
@@ -94,24 +113,29 @@ export default function Header() {
           }
           tabIndex={"-1"}
           target="_blank"
-          className="group/button group inline-flex h-8 items-center rounded-full px-3 text-sm text-zinc-400 outline outline-2 -outline-offset-1 outline-[#f1f5f9] transition-all hover:bg-[#f1f5f9] hover:no-underline focus:rounded-full focus:outline-0 focus:ring-0 sm:hover:text-black sm:hover:outline sm:hover:outline-2 sm:hover:outline-white"
         >
-          <svg className="mr-1 size-5">
-            <use href={`./icons/sprite.svg#tabler/file-outline`} />
-          </svg>
-          Resume
+          <Button
+            variant="gooeyLeft"
+            className="h-8 px-3 rounded-full bg-inherit"
+            ariaLabel="Resume"
+          >
+            <svg className="mr-1 size-5">
+              <use href={`./icons/sprite.svg#tabler/file-outline`} />
+            </svg>
+            Resume
+          </Button>
         </Link>
       </div>
       {showMenu || (
         <>
           <svg
-            className="fixed left-0 top-0 z-20 ml-10 mt-10 size-9 cursor-pointer text-white sm:hidden"
+            className="fixed top-0 left-0 z-20 mt-10 ml-10 text-white cursor-pointer size-9 sm:hidden"
             onClick={() => setShowMenu((prev) => !prev)}
             aria-label="exit button"
           >
             <use href={`./icons/sprite.svg#tabler/x-outline`} />
           </svg>
-          <nav className="fixed left-1/2 top-1/2 z-10 grid min-h-screen w-screen -translate-x-1/2 -translate-y-1/2 place-items-center bg-gradient-to-br from-gray-800 to-gray-950 text-center text-lg text-zinc-400 sm:hidden">
+          <nav className="fixed z-10 grid w-screen min-h-screen text-lg text-center -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 place-items-center bg-gradient-to-br from-gray-800 to-gray-950 text-zinc-400 sm:hidden">
             <ul className="space-y-3">
               <li>
                 <Link href={"/"} onClick={() => setShowMenu((prev) => !prev)}>
