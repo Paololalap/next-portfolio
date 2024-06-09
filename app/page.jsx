78161be1b9ae7368e25d2677859d8850fe252521
Paolo Lalap <paolomorales1502@gmail.com>
2024-06-09 14:48:36 +0800

    update homepage

--- app/page.jsx
+++ app/page.jsx
@@ -9,8 +9,8 @@ import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <SpringFromLeft className="mx-auto mt-8 max-w-2xl">
-      <div className="mt-10 flex items-center gap-x-5">
+    <SpringFromLeft className="max-w-2xl mx-auto mt-8">
+      <div className="flex items-center mt-10 gap-x-5">
         <div className="relative min-h-20 min-w-20">
           <Image
             src="https://avatars.githubusercontent.com/u/84236606?v=4"
@@ -21,8 +21,8 @@ export default function HomePage() {
             sizes="(min-width: 380px) 80px, calc(11.67vw + 38px)"
           />
         </div>
-        <div className="flex w-full flex-col leading-8">
-          <h1 className="text-3xl font-semibold capitalize text-white">
+        <div className="flex flex-col w-full leading-8">
+          <h1 className="text-3xl font-semibold text-white capitalize">
             Paolo <span className="text-[#D62828]">Lalap</span>
           </h1>
           <Typewriter texts={ROLES} />
@@ -33,8 +33,8 @@ export default function HomePage() {
         <p className="mb-8 text-lg">
           I am a Freelancer based in the Philippines <ChevronLeftRight />, and
           I&apos;m very passionate and dedicated to my work <ChevronLeftRight />
-          . With experience in HTML, CSS, React.js, Next.js, Tailwind CSS, React
-          Native, JavaScript, MongoDB, Firebase, and Git <ChevronLeftRight />.
+          . Expertise in HTML, CSS, React.js, Next.js, Tailwind CSS, React
+          Native, JavaScript, Framer Motion, MongoDB, Firebase, and Git <ChevronLeftRight />.
         </p>
         <p className="mb-8 text-lg">
           This is my self-coded personal portfolio which features some of my
@@ -48,7 +48,7 @@ export default function HomePage() {
         <span className="text-[#D62828]">Professional</span> Skill set and tools
         I use
       </h2>
-      <div className="relative flex h-full w-full items-center justify-center overflow-hidden bg-inherit px-20 pb-20">
+      <div className="relative flex items-center justify-center w-full h-full px-20 pb-20 overflow-hidden bg-inherit">
         <IconCloud iconSlugs={ICON_SLUGS} />
       </div>
     </SpringFromLeft>
