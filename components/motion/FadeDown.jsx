5fbab385cc53218f0e8666c2358af7d89cb611bc
Paolo Lalap <paolomorales1502@gmail.com>
2024-06-10 10:57:45 +0800

    rename

--- components/motion/FadeDown.jsx
+++ components/motion/FadeDown.jsx
@@ -1,11 +1,11 @@
 "use client";
 import { motion } from "framer-motion";
 
-export default function SpringFromTop({ children }) {
+export default function FadeDown({ children }) {
   return (
     <motion.div
-      initial={{ y: "-100vh" }}
-      animate={{ y: 0 }}
+      initial={{ y: "-100vh", opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
     >
       {children}
     </motion.div>
