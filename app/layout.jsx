196a8ad5de69c489a20a3261c7ce2949b0a6dcd3
Paolo Lalap <paolomorales1502@gmail.com>
2024-06-11 16:03:48 +0800

    update loading

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,7 @@ import { roboto } from "@/fonts/fonts";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { PreloadResources } from "./preload-resources";
+import { Suspense } from "react";
 
 export const metadata = {
   title: {
@@ -26,9 +27,11 @@ export default function RootLayout({ children }) {
         )}
       >
         <BackgroundBeams />
-        <Header />
-        {children}
-        <Footer />
+        <Suspense fallback={null}>
+          <Header />
+          {children}
+          <Footer />
+        </Suspense>
       </body>
     </html>
   );
--- fonts/fonts.js
+++ fonts/fonts.js
@@ -1,15 +1,8 @@
-import { Lobster, Roboto } from "next/font/google";
+import { Roboto } from "next/font/google";
 
 export const roboto = Roboto({
   subsets: ["latin"],
   display: "swap",
   weight: ["100", "300", "400", "500", "700", "900"],
   variable: "--font-roboto",
-});
-
-export const lobster = Lobster({
-  subsets: ["latin"],
-  display: "swap",
-  weight: ["400"],
-  variable: "--font-lobster",
 });
