fa8ba4e444611109f90699d5de370ee340f419de
Paolo Lalap <paolomorales1502@gmail.com>
2024-06-12 08:54:00 +0800

    add social links button

--- components/SocialLinks.jsx
+++ components/SocialLinks.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import Button from "@/components/button/SocialLinks";
 import { SOCIAL_LINKS } from "@/constants/SOCIAL_LINKS.js";
 import Link from "next/link";
 
@@ -11,10 +11,9 @@ export default function SocialLinks() {
             href={link.href}
             target="_blank"
             tabIndex="-1"
-            className="group space-x-1 text-muted-foreground transition-all after:ml-3 hover:text-primary"
+            className="space-x-1 transition-all group text-muted-foreground after:ml-3 hover:text-primary"
           >
             <Button
-              variant="linkHover1"
               className="p-0 text-base text-muted-foreground group-hover:text-primary"
               aria-label={link.text}
             >
