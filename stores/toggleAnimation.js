a39462746af826d7e7614067e9e43f1624433fdb
Paolo Lalap <paolomorales1502@gmail.com>
2024-06-14 13:34:23 +0800

    update toggleAnimation store

--- stores/toggleAnimation.js
+++ stores/toggleAnimation.js
@@ -1,20 +1,8 @@
-import Cookies from "js-cookie";
 import { create } from "zustand";
 
-// TODO:
-/* function getInitialValueFromCookies() {
-  const value = Cookies.get('toggleAnimation');
-  if (value === undefined) {
-    Cookies.set('toggleAnimation', 'true');
-    return true;
-  }
-  return value === 'true';
-} */
-
 const useStore = create((set) => ({
-  toggleAnimation: /* getInitialValueFromCookies() */ true,
+  toggleAnimation: true,
   setToggleAnimation: (value) => {
-    Cookies.set("toggleAnimation", value.toString());
     set({ toggleAnimation: value });
   },
 }));
