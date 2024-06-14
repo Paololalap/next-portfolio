747ccc12f2ba88d1c45fc8634013616e439c8e8f
Paolo Lalap <paolomorales1502@gmail.com>
2024-06-14 12:43:38 +0800

    add sheet component

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -12,6 +12,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Switch } from "@/components/ui/switch";
 import {
   Tooltip,
@@ -31,11 +32,11 @@ export default function Header() {
   const [hoveredButton, setHoveredButton] = useState(
     pathname === "/" ? "home" : pathname === "/work" ? "work" : "projects",
   );
-  const [showMenu, setShowMenu] = useState(true);
   const { toggleAnimation, setToggleAnimation } = useStore();
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [isClient, setIsClient] = useState(false);
   const isDesktop = useMediaQuery("(min-width: 768px)");
+  const [isSheetOpen, setIsSheetOpen] = useState(false);
 
   useEffect(() => {
     setIsClient(true);
@@ -45,7 +46,7 @@ export default function Header() {
   if (!isClient) return null;
 
   return (
-    <header className="mx-auto mt-8 max-w-2xl">
+    <header className="mx-auto mt-8 max-w-2xl px-8">
       <FadeDown className="flex items-center gap-x-2">
         <div className="flex flex-1 items-center justify-between rounded-full border border-border px-2 py-1 sm:flex-row sm:py-2">
           <nav className="group relative hidden items-center text-base sm:flex">
@@ -157,15 +158,35 @@ export default function Header() {
             <HoverLine activeButton={hoveredButton} />
           </nav>
 
-          <Button
-            className="bg-transparent p-0 hover:bg-transparent sm:hidden"
-            aria-label="menu"
-            onClick={() => setShowMenu(!showMenu)}
-          >
-            <svg className="ml-1 size-7 text-foreground">
-              <use href={`./icons/sprite.svg#tabler/menu-2-outline`} />
-            </svg>
-          </Button>
+          <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
+            <SheetTrigger className="sm:hidden">
+              <svg className="ml-1 size-7 text-foreground">
+                <use href={`./icons/sprite.svg#tabler/menu-2-outline`} />
+              </svg>
+            </SheetTrigger>
+            <SheetContent className="grid place-items-center">
+              <ul className="space-y-3 text-center text-lg text-muted-foreground">
+                <li>
+                  <Link href={"/"} onClick={() => setIsSheetOpen(false)}>
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link href={"/work"} onClick={() => setIsSheetOpen(false)}>
+                    Work
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={"/projects"}
+                    onClick={() => setIsSheetOpen(false)}
+                  >
+                    Projects
+                  </Link>
+                </li>
+              </ul>
+            </SheetContent>
+          </Sheet>
 
           <Link
             href={
@@ -229,42 +250,6 @@ export default function Header() {
           </DialogContent>
         </Dialog>
       </FadeDown>
-      {showMenu || (
-        <>
-          <svg
-            className="fixed left-0 top-0 z-20 ml-10 mt-10 size-9 cursor-pointer text-foreground sm:hidden"
-            onClick={() => setShowMenu((prev) => !prev)}
-            aria-label="exit button"
-          >
-            <use href={`./icons/sprite.svg#tabler/x-outline`} />
-          </svg>
-          <nav className="fixed left-1/2 top-1/2 z-10 grid min-h-screen w-screen -translate-x-1/2 -translate-y-1/2 place-items-center bg-background text-center text-lg text-muted-foreground sm:hidden">
-            <ul className="space-y-3">
-              <li>
-                <Link href={"/"} onClick={() => setShowMenu((prev) => !prev)}>
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href={"/work"}
-                  onClick={() => setShowMenu((prev) => !prev)}
-                >
-                  Work
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href={"/projects"}
-                  onClick={() => setShowMenu((prev) => !prev)}
-                >
-                  Projects
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </>
-      )}
     </header>
   );
 }
