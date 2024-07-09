0d245da05e0eb88ac7f98224aee0865a5b4f6c2a
paololalap <paolomorales1502@gmail.com>
2024-07-09 23:37:20 +0800

    update work page

---
+++ constants/WORK_EXPERIENCE.js
@@ -0,0 +1,40 @@
+import { LAGUNA, SYDNEY } from "./WORK";
+
+export const WORK_EXPERIENCE = [
+  {
+    location: "Laguna, Philippines (Onsite)",
+    duration: "Jul 2024 - Present",
+    title: "Junior Office Aide",
+    company: "at Open University",
+    help: "Why did I get this job?",
+    helpDescription:
+      "I couldn't find a job related to web development, so I have temporarily accepted this position. I didn't want to waste time waiting for the perfect job, so I decided to take this position.",
+    description:
+      "My responsibilities include answering phones, filing documents <ChevronLeftRight />, data entry, managing correspondence, scheduling appointments, and assisting with office organization <ChevronLeftRight />. I handle mail distribution <ChevronLeftRight />, photocopying, and other routine office duties, playing a crucial role in ensuring the smooth operation of the office by supporting other staff members and maintaining a tidy and efficient workspace <ChevronLeftRight />.",
+  },
+  {
+    location: "Sydney, Australia (Remote)",
+    duration: "(Commission)",
+    title: "FullStack Web Developer",
+    description:
+      "Tasked with creating websites to replicate designs in Figma <ChevronLeftRight /> ensuring that all elements match those in Figma and are also responsive <ChevronLeftRight />. Additionally, responsible for implementing backend functionality <ChevronLeftRight />, such as authentication <ChevronLeftRight />.",
+    skills: SYDNEY,
+  },
+  {
+    location: "Laguna, Philippines (Onsite)",
+    duration: "Feb 2024 - May 2024",
+    title: "FullStack Web Developer Intern",
+    company: "at Open University",
+    description:
+      "Tasked with creating and building websites and platforms <ChevronLeftRight />, as well as implementing backend features to ensure smooth operation across various devices and popular web browsers <ChevronLeftRight />.",
+    skills: LAGUNA,
+  },
+  {
+    location: "Laguna, Philippines (Onsite)",
+    duration: "Oct 2019 - Mar 2020",
+    title: "Data Entry Specialist Intern",
+    company: "at Pamana Hospital",
+    description:
+      "Assembling images, typography, or motion graphics to create a piece of design <ChevronLeftRight />. Creating the graphics primarily for published, printed, or electronic media, such as brochures and advertising <ChevronLeftRight />.",
+  },
+];
