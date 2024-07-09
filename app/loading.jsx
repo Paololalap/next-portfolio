ceaec745da272b9d0f1402bc92f8e806c8d4a2b3
paololalap <paolomorales1502@gmail.com>
2024-07-09 23:33:56 +0800

    organize styles

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,17 +1,10 @@
 export default function Loading() {
   return (
     <div
-      className="grid w-screen overflow-hidden place-items-center"
+      className="grid w-screen place-items-center overflow-hidden"
       style={{ height: `calc(100vh - 82px)` }}
     >
-      <span
-        className={`
-      relative size-16 rotate-45 overflow-hidden bg-background 
-      
-      before:absolute before:inset-[-15px] before:m-auto before:animate-diamondLoader before:bg-primary
-
-      after:absolute after:inset-[8px] after:m-auto after:bg-[#222b32] `}
-      />
+      <span className="relative size-16 rotate-45 overflow-hidden bg-background before:absolute before:inset-[-15px] before:m-auto before:animate-diamondLoader before:bg-primary after:absolute after:inset-[8px] after:m-auto after:bg-[#222b32]" />
     </div>
   );
 }
